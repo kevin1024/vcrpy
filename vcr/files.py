29a793a0d208c725e8d8e2f24a3fd47deb0874e9
Kevin McCarthy <me@kevinmccarthy.org>
2012-05-30 23:11:14 -1000

    PEP8 cleanup and make files.py work in python2.5

--- vcr/files.py
+++ vcr/files.py
@@ -1,5 +1,7 @@
+from __future__ import with_statement
 import os
 import yaml
+
 
 def load_cassette(cassette_path):
     try:
@@ -7,11 +9,10 @@ def load_cassette(cassette_path):
     except IOError:
         return None
 
-def save_cassette(cassette_path,cassette):
-    dirname,filename = os.path.split(cassette_path)
+
+def save_cassette(cassette_path, cassette):
+    dirname, filename = os.path.split(cassette_path)
     if not os.path.exists(dirname):
         os.makedirs(dirname)
-    with open(cassette_path,'wc') as cassette_file:
+    with open(cassette_path, 'wc') as cassette_file:
         cassette_file.write(cassette.serialize())
-
-
