38d71080ead4911fdda634e7416d795df23d49c4
Kevin McCarthy <me@kevinmccarthy.org>
2012-05-30 21:52:01 -1000

    headers

--- test.py
+++ test.py
@@ -15,14 +15,22 @@ class TestHttpRequest(unittest.TestCase):
             pass
 
     def test_response_code(self):
+        code = urllib2.urlopen('http://www.iana.org/domains/example/').getcode()
         with vcr.use_cassette(TEST_CASSETTE_FILE):
-            code = urllib2.urlopen('http://www.iana.org/domains/example/').getcode()
+            self.assertEqual(code, urllib2.urlopen('http://www.iana.org/domains/example/').getcode())
             self.assertEqual(code, urllib2.urlopen('http://www.iana.org/domains/example/').getcode())
 
     def test_response_body(self):
+        body = urllib2.urlopen('http://www.iana.org/domains/example/').read()
         with vcr.use_cassette('test/synopsis.yaml'):
-            body = urllib2.urlopen('http://www.iana.org/domains/example/').read()
             self.assertEqual(body, urllib2.urlopen('http://www.iana.org/domains/example/').read())
+            self.assertEqual(body, urllib2.urlopen('http://www.iana.org/domains/example/').read())
+
+    def test_response_headers(self):
+        headers = urllib2.urlopen('http://www.iana.org/domains/example/').info()
+        with vcr.use_cassette('test/synopsis.yaml'):
+            self.assertEqual(headers, urllib2.urlopen('http://www.iana.org/domains/example/').info())
+            self.assertEqual(headers, urllib2.urlopen('http://www.iana.org/domains/example/').info())
 
 
 if __name__ == '__main__':
