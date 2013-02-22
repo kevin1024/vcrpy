2db56648090bf077eb6907f814bead9cd3184826
Kevin McCarthy <me@kevinmccarthy.org>
2013-02-21 22:57:35 -1000

    add better pypi classifiers

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ import sys
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
+
 class PyTest(TestCommand):
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import pytest
@@ -16,20 +19,23 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 setup(name='vcrpy',
-    version='0.0.2',
-    description="A Python port of Ruby's VCR to make mocking HTTP easier",
-    author='Kevin McCarthy',
-    author_email='me@kevinmccarthy.org',
-    url='https://github.com/kevin1024/vcrpy',
-    packages=['vcr'],
-    install_requires=['PyYAML'],
-    license='MIT',
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-    ],
+      version='0.0.2',
+      description="A Python port of Ruby's VCR to make mocking HTTP easier",
+      author='Kevin McCarthy',
+      author_email='me@kevinmccarthy.org',
+      url='https://github.com/kevin1024/vcrpy',
+      packages=['vcr'],
+      install_requires=['PyYAML'],
+      license='MIT',
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Testing',
+          'Topic :: Internet :: WWW/HTTP',
+          'License :: OSI Approved :: MIT License',
+      ],
 )
