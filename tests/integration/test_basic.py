1a73334b50147de720979fc45d6571270b2fc594
Kevin McCarthy <me@kevinmccarthy.org>
2013-08-10 16:18:27 -1000

    update tests

--- tests/integration/test_basic.py
+++ tests/integration/test_basic.py
@@ -1,34 +1,32 @@
 '''Basic tests about cassettes'''
 # coding=utf-8
 
-# Internal imports
-import vcr
-from .common import TestVCR
-
 # External imports
 import os
 import urllib2
 
+# Internal imports
+import vcr
 
-class TestCassette(TestVCR):
-    '''We should be able to save a cassette'''
-    fixtures = os.path.join('tests', 'fixtures', 'basic')
+def test_nonexistent_directory(tmpdir):
+    '''If we load a cassette in a nonexistent directory, it can save ok'''
+    # Check to make sure directory doesnt exist
+    assert not os.path.exists(str(tmpdir.join('nonexistent')))
 
-    def test_nonexistent_directory(self):
-        '''If we load a cassette in a nonexistent directory, it can save ok'''
-        self.assertFalse(os.path.exists(self.fixture('nonexistent')))
-        with vcr.use_cassette(self.fixture('nonexistent', 'cass.yaml')):
-            urllib2.urlopen('http://httpbin.org/').read()
-        # This should have made the file and the directory
-        self.assertTrue(
-            os.path.exists(self.fixture('nonexistent', 'cass.yaml')))
-
-    def test_unpatch(self):
-        '''Ensure that our cassette gets unpatched when we're done'''
-        with vcr.use_cassette(self.fixture('unpatch.yaml')) as cass:
-            urllib2.urlopen('http://httpbin.org/').read()
-
-        # Make the same requests, and assert that we haven't served any more
-        # requests out of cache
+    # Run VCR to create dir and cassette file
+    with vcr.use_cassette(str(tmpdir.join('nonexistent','cassette.yml'))):
         urllib2.urlopen('http://httpbin.org/').read()
-        self.assertEqual(cass.play_count, 0)
+
+    # This should have made the file and the directory
+    assert os.path.exists(str(tmpdir.join('nonexistent','cassette.yml')))
+
+def test_unpatch(tmpdir):
+    '''Ensure that our cassette gets unpatched when we're done'''
+    with vcr.use_cassette(str(tmpdir.join('unpatch.yaml'))) as cass:
+        urllib2.urlopen('http://httpbin.org/').read()
+
+    # Make the same request, and assert that we haven't served any more
+    # requests out of cache
+    urllib2.urlopen('http://httpbin.org/').read()
+    assert cass.play_count == 0
+
