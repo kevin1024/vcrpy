ce88331ebe24bc6e2ed748e2e89aed249414eeab
Kevin McCarthy <me@kevinmccarthy.org>
2013-08-11 15:05:19 -1000

    add test for redirects

---
+++ tests/integration/test_request.py
@@ -0,0 +1,9 @@
+import urllib2
+import vcr
+
+def test_recorded_request_url_with_redirected_request(tmpdir):
+    with vcr.use_cassette(str(tmpdir.join('test.yml'))) as cass:
+        urllib2.urlopen('http://google.com')
+        assert cass.requests[0].url == 'http://google.com'
+        assert cass.requests[1].url == 'http://www.google.com/'
+        assert len(cass) == 2
