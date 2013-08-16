7ad2f10a5eed51a44270819545590fa91ba9be74
Thomas Grainger <thomasgrainger@graingert.co.uk>
2013-08-16 10:31:32 +0100

    fix redirect test
    google changed how they redirect: http://webapps.stackexchange.com/questions/46591/what-does-gws-rd-cr-in-the-url-indicate

--- tests/integration/test_request.py
+++ tests/integration/test_request.py
@@ -4,9 +4,15 @@ import vcr
 def test_recorded_request_url_with_redirected_request(tmpdir):
     with vcr.use_cassette(str(tmpdir.join('test.yml'))) as cass:
         assert len(cass) == 0
-        urllib2.urlopen('http://google.com')
-        print cass.requests
-        print cass.requests[0]
-        assert cass.requests[0].url == 'http://google.com'
-        assert cass.requests[1].url == 'http://www.google.com/'
-        assert len(cass) == 2
+        num_requests = 3
+        urllib2.urlopen(
+            'https://httpbin.org/redirect/{i}'.format(
+                i=num_requests
+            )
+        )
+        for i, j in zip(range(num_requests),range(num_requests, 0, -1)):
+            assert cass.requests[i].url == 'https://httpbin.org/redirect/{i}'.format(
+                i=j
+            )
+        assert cass.requests[-1].url == 'https://httpbin.org/get'
+        assert len(cass) == num_requests+1
