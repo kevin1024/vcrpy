3e247a2efb7e1458274870792bcf1828415e2184
Kevin McCarthy <me@kevinmccarthy.org>
2013-08-19 19:34:38 -1000

    remove errant print statements

--- tests/integration/test_request.py
+++ tests/integration/test_request.py
@@ -5,8 +5,6 @@ def test_recorded_request_url_with_redirected_request(tmpdir):
     with vcr.use_cassette(str(tmpdir.join('test.yml'))) as cass:
         assert len(cass) == 0
         urllib2.urlopen('http://httpbin.org/redirect/3')
-        print cass.requests
-        print cass.requests[0]
         assert cass.requests[0].url == 'http://httpbin.org/redirect/3'
         assert cass.requests[3].url == 'http://httpbin.org/get'
         assert len(cass) == 4
