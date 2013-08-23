726478096038ef16d2f32e215c0bd5c8ec482b20
Kevin McCarthy <me@kevinmccarthy.org>
2013-08-22 18:38:25 -1000

    remove unused cassette in with statement

--- tests/integration/test_config.py
+++ tests/integration/test_config.py
@@ -6,7 +6,7 @@ import vcr
 def test_set_serializer_default_config(tmpdir):
     my_vcr = vcr.VCR(serializer='json')
 
-    with my_vcr.use_cassette(str(tmpdir.join('test.json'))) as cass:
+    with my_vcr.use_cassette(str(tmpdir.join('test.json'))):
         assert my_vcr.serializer == 'json'
         urllib2.urlopen('http://httpbin.org/get')
 
