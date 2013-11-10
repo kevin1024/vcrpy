c2d857c585d86f1286ad2b2947d3b8ae450304a8
Kevin McCarthy <me@kevinmccarthy.org>
2013-11-10 12:25:59 -1000

    Remove Secure File Overwrite Support
    Closes #42

--- vcr/persisters/filesystem.py
+++ vcr/persisters/filesystem.py
@@ -4,20 +4,9 @@ import os
 
 class FilesystemPersister(object):
     @classmethod
-    def _secure_write(cls, path, contents):
-        """
-        We'll overwrite the old version securely by writing out a temporary
-        version and then moving it to replace the old version
-        """
-        dirname, filename = os.path.split(path)
-        fd, name = tempfile.mkstemp(dir=dirname, prefix=filename)
-        with os.fdopen(fd, 'w') as fout:
-            fout.write(contents)
-        os.rename(name, path)
-
-    @classmethod
     def write(cls, cassette_path, data):
         dirname, filename = os.path.split(cassette_path)
         if dirname and not os.path.exists(dirname):
             os.makedirs(dirname)
-        cls._secure_write(cassette_path, data)
+	with open(cassette_path, 'w') as f:
+	    f.write(data)
