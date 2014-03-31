792d6658932506c21849a67f6cca951eac8b83a1
Max Shytikov <mshytikov@gmail.com>
2014-04-01 00:21:01 +0200

    Added unit test for matcher 'url'

--- tests/unit/test_matchers.py
+++ tests/unit/test_matchers.py
@@ -11,3 +11,20 @@ def test_method():
 
     req_post = request.Request('http', 'google.com', 80, 'POST', '/', '', {})
     assert False == matchers.method(req_get, req_post)
+
+
+def test_url():
+    req1 = request.Request('http', 'google.com', 80, 'GET', '/', '', {})
+    assert True == matchers.url(req1, req1)
+
+    req2 = request.Request('http', 'httpbin.org', 80, 'GET', '/', '', {})
+    assert False == matchers.url(req1, req2)
+
+    req1_post = request.Request('http', 'google.com', 80, 'POST', '/', '', {})
+    assert True == matchers.url(req1, req1_post)
+
+    req_query_string = request.Request(
+        'http', 'google.com?p1=t1&p2=t2', 80, 'GET', '/', '', {})
+    req_query_string1 = request.Request(
+        'http', 'google.com?p2=t2&p1=t1', 80, 'GET', '/', '', {})
+    assert False == matchers.url(req_query_string, req_query_string1)
