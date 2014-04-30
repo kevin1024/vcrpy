65c2797f942a1f9c3a94bb82928f20ffc080427f
Max Shytikov <mshytikov@gmail.com>
2014-04-30 03:48:29 +0200

    Updated test for filters. Mock replaced with real Request object

--- tests/unit/test_filters.py
+++ tests/unit/test_filters.py
@@ -1,28 +1,37 @@
-import mock
 from vcr.filters import _remove_headers, _remove_query_parameters
 from vcr.request import Request
 
 
 def test_remove_headers():
-    request = mock.Mock(headers=[('hello','goodbye'),('secret','header')])
-    assert _remove_headers(request, ['secret']).headers == frozenset([('hello','goodbye')])
+    headers = {'hello': ['goodbye'], 'secret': ['header']}
+    request = Request('GET', 'http://google.com', '', headers)
+    _remove_headers(request, ['secret'])
+    assert request.headers == {'hello': ['goodbye']}
 
 
 def test_remove_headers_empty():
-    request = mock.Mock(headers=[('hello','goodbye'),('secret','header')])
-    assert _remove_headers(request, []).headers == frozenset([('hello','goodbye'),('secret','header')])
+    headers = {'hello': ['goodbye'], 'secret': ['header']}
+    request = Request('GET', 'http://google.com', '', headers)
+    _remove_headers(request, [])
+    assert request.headers == headers
 
 
 def test_remove_query_parameters():
-    request = mock.Mock(url='http://g.com/?q=cowboys&w=1')
-    assert _remove_query_parameters(request, ['w']).path == '/?q=cowboys'
+    uri = 'http://g.com/?q=cowboys&w=1'
+    request = Request('GET', uri, '', {})
+    _remove_query_parameters(request, ['w'])
+    assert request.uri == 'http://g.com/?q=cowboys'
 
 
 def test_remove_all_query_parameters():
-    request = mock.Mock(url='http://g.com/?q=cowboys&w=1')
-    assert _remove_query_parameters(request, ['w','q']).path == '/'
+    uri = 'http://g.com/?q=cowboys&w=1'
+    request = Request('GET', uri, '', {})
+    _remove_query_parameters(request, ['w', 'q'])
+    assert request.uri == 'http://g.com/'
 
 
 def test_remove_nonexistent_query_parameters():
-    request = mock.Mock(url='http://g.com/')
-    assert _remove_query_parameters(request, ['w','q']).path == '/'
+    uri = 'http://g.com/'
+    request = Request('GET', uri, '', {})
+    _remove_query_parameters(request, ['w', 'q'])
+    assert request.uri == 'http://g.com/'
