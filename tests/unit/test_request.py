61e3bdc40239cfb5e83b9ef068c2dfa97b9fafa5
Max Shytikov <mshytikov@gmail.com>
2014-05-03 22:31:30 +0200

    Added tetst for uri and port of Request

--- tests/unit/test_request.py
+++ tests/unit/test_request.py
@@ -19,3 +19,19 @@ def test_flat_headers_dict():
     headers = {'X-Header1': ['h1', 'h11'], 'X-Header2': ['h2']}
     req = Request('GET', 'http://go.com:80/', '', headers)
     assert req.flat_headers_dict() == {'X-Header1': 'h1', 'X-Header2': 'h2'}
+
+
+def test_port():
+    req = Request('GET', 'http://go.com/', '', {})
+    assert req.port == 80
+
+    req = Request('GET', 'http://go.com:3000/', '', {})
+    assert req.port == 3000
+
+
+def test_uri():
+    req = Request('GET', 'http://go.com/', '', {})
+    assert req.uri == 'http://go.com/'
+
+    req = Request('GET', 'http://go.com:80/', '', {})
+    assert req.uri == 'http://go.com:80/'
