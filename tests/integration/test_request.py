0b1aeac25ecdfb5bc56474ca8d7657de9d6581ab
Max Shytikov <mshytikov@gmail.com>
2014-05-04 00:01:30 +0200

    Renamed outdated url to uri.

--- tests/integration/test_request.py
+++ tests/integration/test_request.py
@@ -2,10 +2,10 @@ import vcr
 from six.moves.urllib.request import urlopen
 
 
-def test_recorded_request_url_with_redirected_request(tmpdir):
+def test_recorded_request_uri_with_redirected_request(tmpdir):
     with vcr.use_cassette(str(tmpdir.join('test.yml'))) as cass:
         assert len(cass) == 0
         urlopen('http://httpbin.org/redirect/3')
-        assert cass.requests[0].url == 'http://httpbin.org/redirect/3'
-        assert cass.requests[3].url == 'http://httpbin.org/get'
+        assert cass.requests[0].uri == 'http://httpbin.org/redirect/3'
+        assert cass.requests[3].uri == 'http://httpbin.org/get'
         assert len(cass) == 4
