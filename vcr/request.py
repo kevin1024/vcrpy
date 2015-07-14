kevin1024/vcrpy

commit 1deb078ad8db8f678962b031e82444730674ca17

Deserialize request bodies to byte string, not text string (fixes #163).

vcr/serialize.py
@@ -41,9 +41,13 @@ def deserialize(cassette_string, serializer):
     if _looks_like_an_old_cassette(data):
         _warn_about_old_cassette_format()
 
-    requests = [Request._from_dict(r['request']) for r in data['interactions']]
+    requests = []
     responses = []
     for interaction in data['interactions']:
+        request = Request._from_dict(interaction['request'])
+        request.body = compat.convert_body_to_bytes(request.body)
+        requests.append(request)
+
         response = interaction['response']
         try:
             body = response['body']['string']

tests/fixtures/py2body_cassette.yaml
@@ -1,0 +1,16 @@
+interactions:
+- request:
+    body: x=5&y=2
+    headers:
+      Content-Length: ['7']
+      Content-Type: [application/x-www-form-urlencoded]
+      Host: [httpbin.org]
+    method: POST
+    uri: http://httpbin.org/post
+  response:
+    body: {string: !!python/unicode "{}\n"}
+    headers:
+      content-length: ['3']
+      content-type: [application/json]
+    status: {code: 200, message: OK}
+version: 1

tests/fixtures/py3body_cassette.yaml
@@ -1,0 +1,17 @@
+interactions:
+- request:
+    body: !!binary |
+      eD01Jnk9Mg==
+    headers:
+      Content-Length: ['7']
+      Content-Type: [application/x-www-form-urlencoded]
+      Host: [httpbin.org]
+    method: POST
+    uri: http://httpbin.org/post
+  response:
+    body: {string: "{}\n"}
+    headers:
+      content-length: ['3']
+      content-type: [application/json]
+    status: {code: 200, message: OK}
+version: 1

tests/unit/test_serialize.py
@@ -27,6 +27,22 @@ def test_deserialize_new_json_cassette():
         deserialize(f.read(), jsonserializer)
 
 
+def test_deserialize_py2_yaml_cassette():
+    # A cassette generated under Python 2 stores the body as a string, but
+    # the same cassette generated under Python 3 stores it as "!!binary".
+    # Make sure we accept the Python 2 cassette, regardless of whether
+    # we're running under Python 2 or 3.
+    with open('tests/fixtures/py2body_cassette.yaml') as f:
+        (requests, responses) = deserialize(f.read(), yamlserializer)
+    assert requests[0].body == b'x=5&y=2'
+
+def test_deserialize_py3_yaml_cassette():
+    # Same as previous test, except make sure a cassette written under
+    # Python 3 works under both 2 and 3.
+    with open('tests/fixtures/py3body_cassette.yaml') as f:
+        (requests, responses) = deserialize(f.read(), yamlserializer)
+    assert requests[0].body == b'x=5&y=2'
+
 @mock.patch.object(jsonserializer.json, 'dumps',
                    side_effect=UnicodeDecodeError('utf-8', b'unicode error in serialization',
                                                   0, 10, 'blew up'))

commit 99770a7b42bc5e1506e97f11401c949329d15ced

Handle request body the same way, whether it comes from file or string. (issue #163)
This fixes the test failure I introduced in 1deb078ad8db. In that case, running

  request.body = somefunc(request.body)

when request.body came from a file caused weird stuff to happen: under
Python 2, request.body ended up being the repr() of a StringIO object.
Looks like repeated assignment to request.body is not a good idea.

The fix is to ensure request.body is always a byte string in the
Request class itself.

vcr/request.py
@@ -1,4 +1,4 @@
-from six import BytesIO, binary_type
+from six import BytesIO, text_type
 from six.moves.urllib.parse import urlparse, parse_qsl
 
 
@@ -29,11 +29,9 @@ class Request(object):
         self.uri = uri
         self._was_file = hasattr(body, 'read')
         if self._was_file:
-            self._body = body.read()
-            if not isinstance(self._body, binary_type):
-                self._body = self._body.encode('utf-8')
+            self.body = body.read()
         else:
-            self._body = body
+            self.body = body
         self.headers = {}
         for key in headers:
             self.add_header(key, headers[key])
@@ -44,6 +42,8 @@ class Request(object):
 
     @body.setter
     def body(self, value):
+        if isinstance(value, text_type):
+            value = value.encode('utf-8')
         self._body = value
 
     def add_header(self, key, value):

vcr/serialize.py
@@ -44,9 +44,7 @@ def deserialize(cassette_string, serializer):
     requests = []
     responses = []
     for interaction in data['interactions']:
-        request = Request._from_dict(interaction['request'])
-        request.body = compat.convert_body_to_bytes(request.body)
-        requests.append(request)
+        requests.append(Request._from_dict(interaction['request']))
 
         response = interaction['response']
         try: