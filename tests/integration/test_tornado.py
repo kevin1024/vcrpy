01f1f9fdc1fece6b15c164f4089f0c7c3178b8a0
Maarten van Schaik <maarten@byte.nl>
2015-08-14 12:29:50 +0200

    Verify effective_url is ok

--- tests/integration/test_tornado.py
+++ tests/integration/test_tornado.py
@@ -87,6 +87,7 @@ def test_effective_url(get_client, scheme, tmpdir):
     url = scheme + '://httpbin.org/redirect-to?url=/html'
     with vcr.use_cassette(str(tmpdir.join('url.yaml'))):
         effective_url = (yield get(get_client(), url)).effective_url
+        assert effective_url == scheme + '://httpbin.org/html'
 
     with vcr.use_cassette(str(tmpdir.join('url.yaml'))) as cass:
         assert effective_url == (yield get(get_client(), url)).effective_url
