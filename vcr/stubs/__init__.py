8a06e6547f70976bf84d3a4df383ed58783be6d8
Osvaldo Santana Neto <github@osantana.me>
2015-12-16 00:02:34 -0200

    Fix a TypeError in concatenation when request.body is empty

--- vcr/stubs/__init__.py
+++ vcr/stubs/__init__.py
@@ -191,7 +191,7 @@ class VCRConnection(object):
         body of the request.  So if that happens, let's just append the data
         onto the most recent request in the cassette.
         '''
-        self._vcr_request.body = (self._vcr_request.body or '') + data
+        self._vcr_request.body = (self._vcr_request.body or b'') + data
 
     def close(self):
         # Note: the real connection will only close if it's open, so
