889edccecb28d0308f381b38ad33c535c1d4b436
John Vandenberg <jayvdb@gmail.com>
2016-05-02 08:55:35 +0700

    Remove pytest-localserver from test dependencies
    pytest-localserver is no longer needed after
    the switch to using pytest-httpbin.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     install_requires=install_requires,
     extras_require=extras_require,
     license='MIT',
-    tests_require=['pytest', 'mock', 'pytest-localserver', 'pytest-httpbin'],
+    tests_require=['pytest', 'mock', 'pytest-httpbin'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
