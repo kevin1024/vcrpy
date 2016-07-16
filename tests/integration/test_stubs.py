d2368eb2c4e921789736557413cff3cca5a4cb91
Kevin McCarthy <me@kevinmccarthy.org>
2016-07-16 08:58:07 -1000

    fix flaky test

--- tests/integration/test_stubs.py
+++ tests/integration/test_stubs.py
@@ -67,8 +67,8 @@ def test_original_decoded_response_is_not_modified(tmpdir, httpbin):
         assert 'gzip' == inside.headers['content-encoding']
 
         # They should effectively be the same response.
-        inside_headers = (h for h in inside.headers.items() if h[0] != 'Date')
-        outside_headers = (h for h in outside.getheaders() if h[0] != 'Date')
+        inside_headers = (h for h in inside.headers.items() if h[0].lower() != 'date')
+        outside_headers = (h for h in outside.getheaders() if h[0].lower() != 'date')
         assert set(inside_headers) == set(outside_headers)
         inside = zlib.decompress(inside.read(), 16+zlib.MAX_WBITS)
         outside = zlib.decompress(outside.read(), 16+zlib.MAX_WBITS)
