5148381a86083df016a1548143287b70324380f6
Charly <carlos_ds730@yahoo.es>
2017-01-04 20:35:43 -0500

    fixed a test

--- tests/unit/test_stubs.py
+++ tests/unit/test_stubs.py
@@ -11,7 +11,7 @@ class TestVCRConnection(object):
 
     @mock.patch('vcr.cassette.Cassette.can_play_response_for', return_value=False)
     def testing_connect(*args):
-        vcr_connection = VCRHTTPSConnection('www.examplehost.com')
+        vcr_connection = VCRHTTPSConnection('www.google.com')
         vcr_connection.cassette = Cassette('test', record_mode='all')
         vcr_connection.real_connection.connect()
         assert vcr_connection.real_connection.sock is not None
