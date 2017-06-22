c55d976277e0d194a5a774a1575d612e51536718
Allisson Azevedo <allisson@gmail.com>
2017-06-22 15:12:58 -0300

    Update aiohttp_stub to work with binary content

--- vcr/stubs/aiohttp_stubs/__init__.py
+++ vcr/stubs/aiohttp_stubs/__init__.py
@@ -22,6 +22,10 @@ class MockClientResponse(ClientResponse):
         return self.content.decode(encoding)
 
     @asyncio.coroutine
+    def read(self):
+        return self.content
+
+    @asyncio.coroutine
     def release(self):
         pass
 
@@ -71,7 +75,7 @@ def vcr_request(cassette, real_request):
                 'message': response.reason,
             },
             'headers': dict(response.headers),
-            'body': {'string': (yield from response.text())},  # NOQA: E999
+            'body': {'string': (yield from response.read())},  # NOQA: E999
             'url': response.url,
         }
         cassette.append(vcr_request, vcr_response)
