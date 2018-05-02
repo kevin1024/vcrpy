f1f8ce2af479f931c9e9143f400ec2a811191fb7
Luiz Menezes <luiz.menezesf@gmail.com>
2018-05-02 09:07:14 -0300

    Ignore syntax error on async stuff

--- tests/integration/aiohttp_utils.py
+++ tests/integration/aiohttp_utils.py
@@ -1,8 +1,8 @@
 import aiohttp
 
 
-async def aiohttp_request(loop, method, url, output='text', **kwargs):
-    with aiohttp.ClientSession(loop=loop) as session:
+async def aiohttp_request(loop, method, url, output='text', **kwargs):  # NOQA: E999
+    async with aiohttp.ClientSession(loop=loop) as session:  # NOQA: E999
         response = await session.request(method, url, **kwargs)  # NOQA: E999
         if output == 'text':
             content = await response.text()  # NOQA: E999
