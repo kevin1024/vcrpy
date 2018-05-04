6ab508d67d5e178abbf961479cab25beb0b6dc32
Luiz Menezes <luiz.menezesf@gmail.com>
2018-05-04 15:45:11 -0300

    Fix aiohttp_request to properly perform aiohttp requests

--- tests/integration/aiohttp_utils.py
+++ tests/integration/aiohttp_utils.py
@@ -3,11 +3,11 @@ import aiohttp
 
 async def aiohttp_request(loop, method, url, output='text', **kwargs):  # NOQA: E999
     async with aiohttp.ClientSession(loop=loop) as session:  # NOQA: E999
-        response = await session.request(method, url, **kwargs)  # NOQA: E999
-        if output == 'text':
-            content = await response.text()  # NOQA: E999
-        elif output == 'json':
-            content = await response.json()  # NOQA: E999
-        elif output == 'raw':
-            content = await response.read()  # NOQA: E999
-        return response, content
+        async with session.request(method, url, **kwargs) as response:  # NOQA: E999
+            if output == 'text':
+                content = await response.text()  # NOQA: E999
+            elif output == 'json':
+                content = await response.json()  # NOQA: E999
+            elif output == 'raw':
+                content = await response.read()  # NOQA: E999
+            return response, content
