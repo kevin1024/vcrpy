867fd9ab4b1fdda268903fd16663b4b18732531f
Luiz Menezes <luiz.menezesf@gmail.com>
2018-05-07 10:58:09 -0300

    Ignore flake8 for entire file on aiohttp_utils

--- tests/integration/aiohttp_utils.py
+++ tests/integration/aiohttp_utils.py
@@ -1,15 +1,16 @@
+# flake8: noqa
 import aiohttp
 
 
 async def aiohttp_request(loop, method, url, output='text',
-                          encoding='utf-8', headers=None, **kwargs):  # NOQA: E999
-    async with aiohttp.ClientSession(loop=loop, headers=headers or {}) as session:  # NOQA: E999
-        async with session.request(method, url, **kwargs) as response:  # NOQA: E999
+                          encoding='utf-8', headers=None, **kwargs):
+    async with aiohttp.ClientSession(loop=loop, headers=headers or {}) as session:
+        async with session.request(method, url, **kwargs) as response:
             if output == 'text':
-                content = await response.text()  # NOQA: E999
+                content = await response.text()
             elif output == 'json':
-                content = await response.json(encoding=encoding)  # NOQA: E999
+                content = await response.json(encoding=encoding)
             elif output == 'raw':
-                content = await response.read()  # NOQA: E999
+                content = await response.read()
 
             return response, content
