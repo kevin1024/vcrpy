75cb067e2911aa8fcbb9e10657a79f97925f3e64
Luiz Menezes <luiz.menezesf@gmail.com>
2018-07-07 23:56:39 -0300

    Fix content type being passed to aiohttp response stub

--- tests/integration/aiohttp_utils.py
+++ tests/integration/aiohttp_utils.py
@@ -5,7 +5,7 @@ import aiohttp
 
 
 @asyncio.coroutine
-def aiohttp_request(loop, method, url, output='text', encoding='utf-8', **kwargs):
+def aiohttp_request(loop, method, url, output='text', encoding='utf-8', content_type=None, **kwargs):
     session = aiohttp.ClientSession(loop=loop)
     response_ctx = session.request(method, url, **kwargs)
 
@@ -13,7 +13,8 @@ def aiohttp_request(loop, method, url, output='text', encoding='utf-8', **kwargs
     if output == 'text':
         content = yield from response.text()
     elif output == 'json':
-        content = yield from response.json(encoding=encoding)
+        content_type = content_type or 'application/json'
+        content = yield from response.json(encoding=encoding, content_type=content_type)
     elif output == 'raw':
         content = yield from response.read()
 
