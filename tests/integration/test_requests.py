20a4c18f8006fec7d8feef8f94cdcd131add2121
Arthur Hamon <arthur@lumapps.com>
2019-06-14 12:03:10 +0200

    fix FileModeWarning
    This fix the following warning:
    FileModeWarning: Requests has determined the content-length for this request using the binary size of the file: however, the file has been opened in text mode (i.e. without the 'b' flag in the mode). This may lead to an incorrect content-length. In Requests 3.0, support will be removed for files in text mode.

--- tests/integration/test_requests.py
+++ tests/integration/test_requests.py
@@ -254,7 +254,7 @@ def test_nested_cassettes_with_session_created_before_nesting(httpbin_both, tmpd
 def test_post_file(tmpdir, httpbin_both):
     '''Ensure that we handle posting a file.'''
     url = httpbin_both + '/post'
-    with vcr.use_cassette(str(tmpdir.join('post_file.yaml'))) as cass, open('tox.ini') as f:
+    with vcr.use_cassette(str(tmpdir.join('post_file.yaml'))) as cass, open('tox.ini', 'rb') as f:
         original_response = requests.post(url, f).content
 
     # This also tests that we do the right thing with matching the body when they are files.
