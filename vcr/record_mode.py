bceaab8b88216b5cc9cc52634bf19a1bd5b0c71c
Aaron Bannin <aaron.bannin@curology.com>
2020-05-24 20:44:21 -0700

    add carraige returns

--- vcr/record_mode.py
+++ vcr/record_mode.py
@@ -1,8 +1,11 @@
 from enum import Enum
 
+
 class RecordMode(str, Enum):
     """
     Configues when VCR will record to the cassette.
+
+    Can be declared by either using the enumerated value (`vcr.mode.ONCE`) or by simply using the defined string (`once`).
 
     `ALL`: Every request is recorded.
     `ANY`: ?
@@ -10,6 +13,7 @@ class RecordMode(str, Enum):
     `NONE`: No requests are recorded.
     `ONCE`:  First set of requests is recorded, all others are replayed. Attempting to add a new episode fails.
     """
+    
     ALL = "all"
     ANY = "any"
     NEW_EPISODES = "new_episodes"
