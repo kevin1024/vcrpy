43484e7cff5e525f4707971d9ada22675fbae291
Sebastian Pipping <sebastian@pipping.org>
2023-05-26 01:16:20 +0200

    test_aiohttp.py: Make cookie tests use pytest-httpbin (#706)
    .. to make them faster and more robust.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,3 +1,6 @@
+import os
+import ssl
+
 import pytest
 
 
@@ -15,3 +18,15 @@ def mockbin(scheme):
 @pytest.fixture
 def mockbin_request_url(mockbin):
     return mockbin + "/request"
+
+
+@pytest.fixture
+def httpbin_ssl_context():
+    ssl_ca_location = os.environ["REQUESTS_CA_BUNDLE"]
+    ssl_cert_location = os.environ["REQUESTS_CA_BUNDLE"].replace("cacert.pem", "cert.pem")
+    ssl_key_location = os.environ["REQUESTS_CA_BUNDLE"].replace("cacert.pem", "key.pem")
+
+    ssl_context = ssl.create_default_context(cafile=ssl_ca_location)
+    ssl_context.load_cert_chain(ssl_cert_location, ssl_key_location)
+
+    return ssl_context
