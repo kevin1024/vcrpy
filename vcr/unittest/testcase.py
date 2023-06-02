ad650a7ee1b57ec967d854b320792aa6548fb6c4
Sebastian Pipping <sebastian@pipping.org>
2023-06-02 17:10:23 +0200

    vcr/unittest: Apply black formatting

--- vcr/unittest/testcase.py
+++ vcr/unittest/testcase.py
@@ -2,14 +2,15 @@ import inspect
 import logging
 import os
 import unittest
-from .. import VCR
 
+from .. import VCR
 
 logger = logging.getLogger(__name__)
 
 
 class VCRMixin(object):
     """A TestCase mixin that provides VCR integration."""
+
     vcr_enabled = True
 
     def setUp(self):
@@ -22,8 +23,8 @@ class VCRMixin(object):
             self.addCleanup(cm.__exit__, None, None, None)
 
     def _get_vcr(self, **kwargs):
-        if 'cassette_library_dir' not in kwargs:
-            kwargs['cassette_library_dir'] = self._get_cassette_library_dir()
+        if "cassette_library_dir" not in kwargs:
+            kwargs["cassette_library_dir"] = self._get_cassette_library_dir()
         return VCR(**kwargs)
 
     def _get_vcr_kwargs(self, **kwargs):
@@ -31,11 +32,10 @@ class VCRMixin(object):
 
     def _get_cassette_library_dir(self):
         testdir = os.path.dirname(inspect.getfile(self.__class__))
-        return os.path.join(testdir, 'cassettes')
+        return os.path.join(testdir, "cassettes")
 
     def _get_cassette_name(self):
-        return '{0}.{1}.yaml'.format(self.__class__.__name__,
-                                     self._testMethodName)
+        return "{0}.{1}.yaml".format(self.__class__.__name__, self._testMethodName)
 
 
 class VCRTestCase(VCRMixin, unittest.TestCase):
