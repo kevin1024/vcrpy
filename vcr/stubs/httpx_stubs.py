f002c31171ab464bb62c0fdb2d367ab96577d162
alekseik1 <1alekseik1@gmail.com>
2025-01-27 17:55:53 +0300

    Revert "fix(httpx): don't save response if filtered to None"
    This reverts commit d7633729de317c26c632defde8b25ef1dcf771e6.

--- vcr/stubs/httpx_stubs.py
+++ vcr/stubs/httpx_stubs.py
@@ -136,9 +136,7 @@ async def _record_responses(cassette, vcr_request, real_response, aread):
         # final redirect value
         vcr_request = _make_vcr_request(real_response.request)
 
-    should_save_response = cassette._before_record_request(vcr_request) is not None
-    if should_save_response:
-        cassette.append(vcr_request, await _to_serialized_response(real_response, aread))
+    cassette.append(vcr_request, await _to_serialized_response(real_response, aread))
     return real_response
 
 
